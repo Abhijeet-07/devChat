e6282e12deb8239cb71c976251e730590a3ea4e2
Abhijeet Maurya <mauryaabhijeet8@gmail.com>
2025-07-04 23:10:02 +0530

    init-db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+require("dotenv").config(); // loads .env from the root
 
 const app = express();
 
