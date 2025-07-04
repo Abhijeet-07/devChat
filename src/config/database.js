596219f2ad0616b2fb2c00ba42907ca546688c38
Abhijeet Maurya <mauryaabhijeet8@gmail.com>
2025-07-04 23:13:11 +0530

    none

--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error(" MongoDB connection failed:", error.message);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
